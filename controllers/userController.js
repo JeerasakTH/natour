eada434373643bc7ff16b60c378e21f12d56aebc
Jeerasak Todsungnern <78092831+JeerasakTH@users.noreply.github.com>
2022-06-26 20:40:33 +0700

    Logging in Users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,19 @@
-exports.getAllUsers = (req, res) => {
+const User = require('./../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+  // const users = await Tour.find();
+
+  // SEND RESPONSE
   res.status(200).json({
-    status: 'error',
-    message: 'This route is not yet defined!',
+    status: 'success',
+    results: users.length,
+    data: {
+      users,
+    },
   });
-};
+});
 
 exports.getUser = (req, res) => {
   res.status(200).json({
