ad3ee19d242fd0b9d48be423b237eebc96292935
Jeerasak Todsungnern <78092831+JeerasakTH@users.noreply.github.com>
2022-07-26 18:11:42 +0700

    Complete Modelling Data

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,5 +1,6 @@
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
+const APIFeatures = require('./../utils/apiFeatures');
 
 exports.deleteOne = (Model) =>
   catchAsync(async (req, res, next) => {
@@ -22,22 +23,110 @@ exports.deleteOne = (Model) =>
     // }
   });
 
-// exports.deleteTour = catchAsync(async (req, res, next) => {
-//     const tour = await Tour.findByIdAndDelete(req.params.id);
+exports.updateOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
 
-//     if (!tour) {
-//       return next(new AppError('No tour found with that ID', 404));
-//     }
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
 
-//     res.status(204).json({
-//       status: 'success',
-//       data: null,
-//     });
-// try {
-// } catch (err) {
-//   res.status(404).json({
-//     status: 'fail',
-//     message: err,
-//   });
-// }
-//   });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    });
+    // try {
+    // } catch (err) {
+    //   res.status(404).json({
+    //     status: 'fail',
+    //     message: err,
+    //   });
+    // }
+  });
+
+exports.createOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    });
+    // try {
+    // const newTour = await Tour.create(req.body);
+    // res.status(201).json({
+    //   status: 'success',
+    //   data: {
+    //     tour: newTour,
+    //   },
+    // });
+    // } catch (err) {
+    //   res.status(404).json({
+    //     status: 'fail',
+    //     message: err,
+    //   });
+    // }
+  });
+
+exports.getOne = (Model, popOptions) =>
+  catchAsync(async (req, res, next) => {
+    let query = Model.findById(req.params.id);
+    if (popOptions) query = query.populate(popOptions);
+    const doc = await query;
+
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        doc,
+      },
+    });
+    // try {
+    // } catch (err) {
+    //   res.status(404).json({
+    //     status: 'fail',
+    //     message: err,
+    //   });
+    // }
+  });
+
+exports.getAll = (Model) =>
+  catchAsync(async (req, res, next) => {
+    // To allow for nested Get review on tour (hack)
+    let filter = {};
+    if (req.params.tourId) filter = { tour: req.params.tourId };
+
+    // EXECUTE QUERY
+    const features = new APIFeatures(Model.find(filter), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    // const doc = await features.query.explain();
+    const doc = await features.query;
+
+    // SEND RESPONSE
+    res.status(200).json({
+      status: 'success',
+      results: doc.length,
+      data: {
+        data: doc,
+      },
+    });
+    // try {
+    // } catch (err) {
+    //   res.status(404).json({
+    //     status: 'fail',
+    //     message: err,
+    //   });
+    // }
+  });
